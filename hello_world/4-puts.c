0b0a1d1962c234dc305a9a5a34038562ef1e3a4c
Random13-sketch <agazadenahid98@gmail.com>
2025-11-29 23:42:42 +0400

    Add Betty-doc compliant puts program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a specific message using puts
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
