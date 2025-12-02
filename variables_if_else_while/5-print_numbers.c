9809fd9b069899c1e5cd4baed13bc6366e972cac
Random13-sketch <agazadenahid98@gmail.com>
2025-12-02 12:46:38 +0400

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0 (Success)
  **/
-
+ 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; ++i)
-    {
-        printf("%d", i);
-    }
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d", i);
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
