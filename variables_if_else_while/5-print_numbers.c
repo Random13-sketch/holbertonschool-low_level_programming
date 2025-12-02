ee1824ef3855b5619e2239533624fd78499e46e8
Random13-sketch <agazadenahid98@gmail.com>
2025-12-02 12:40:53 +0400

    Add files via upload

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits (0-9)
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%d", i);
+    }
+    printf("\n");
+	return 0;
+}
