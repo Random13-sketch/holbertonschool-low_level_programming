60521a3177088b1aaf09e07685f817f8b6ea958a
Random13-sketch <agazadenahid98@gmail.com>
2025-12-03 11:00:02 +0400

    .

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,22 +16,19 @@ int main(void)
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
+    printf("Last digit of %d is %d and is ", n, n % 10);
 
-    int d;
-    d = n % 10;
-    printf("Last digit of %d is %d and is ", n, d);
-
-    if (d > 5)
+    if (n % 10 > 5)
     {
-        printf("greater than 5");
+        printf("greater than 5\n");
     }
-    else if (d < 6)
+    else if (n % 10 < 6 && n % 10 != 0)
     {
-        printf("less than 6 and not 0");
+        printf("less than 6 and not 0\n");
     }
     else
     {
-        printf("0");
+        printf("0\n");
     }
     
 	return (0);
