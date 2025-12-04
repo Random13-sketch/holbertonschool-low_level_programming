1f9ae36dc728c668a49d62cf426b5300f97bbf94
Random13-sketch <agazadenahid98@gmail.com>
2025-12-04 15:42:32 +0400

    .

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Prints the solution for the task.
+ *
+ * Returns: 1 if c is lowercase, 0 otherwise
+ **/
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 123);
+}
