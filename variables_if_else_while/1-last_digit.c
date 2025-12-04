5446a16eb3ac853dd5bc5a30d229000ee73b9223
Random13-sketch <agazadenahid98@gmail.com>
2025-12-04 12:12:25 +0400

    .

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,11 +3,8 @@
 #include <time.h>
 
 /**
-*	OBJECTIVE - This program will assign a random number to the variable n
-*	each time it is executed. Complete the source code in order to
-*	print the last digit of the number stored in the variable n.
 *
-*	main - completes the objective logic
+*	main - print the last digit of the number stored in the variable n
 *
 *	return - always 0 (success)
 *
