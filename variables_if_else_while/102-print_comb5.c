16aed8b31383e3c788beba1113a7355a7d39dcec
Random13-sketch <agazadenahid98@gmail.com>
2025-12-04 12:36:40 +0400

    .

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i <= 9; ++i)
 	{
-		for (j = i + 1; j <= 9; ++j)
+		for (j = i j <= 9; ++j)
 		{
-			for (k = j + 1; k <= 9; ++k)
+			for (k = 0; k <= 9; ++k)
 			{
-				for (l = k + 1; l <= 9; ++l)
+				for (l = k; l <= 9; ++l)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
