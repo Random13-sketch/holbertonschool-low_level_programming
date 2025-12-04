9b915ab15f88265718d5ba5313183a160e45e320
Random13-sketch <agazadenahid98@gmail.com>
2025-12-04 15:26:45 +0400

    .

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the solution for the task.
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
