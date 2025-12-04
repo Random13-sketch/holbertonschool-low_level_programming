b0123f697a2043fd3f8eec2eeb721a3e491cd97d
Random13-sketch <agazadenahid98@gmail.com>
2025-12-04 15:29:10 +0400

    .

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,12 @@
+#include <stdio.h>
+
 int _putchar(char c);
-void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z' ++c)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
