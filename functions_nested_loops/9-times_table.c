2da12b4249d35673f4a58498d37c972415002aa5
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 14:08:41 +0400

    .

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the solution for the task.
+ *
+ **/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; ++j)
+		{
+			int val;
+			val = i * j;
+			if (val < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((val % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((val / 10) + '0');
+				_putchar((val % 10) + '0');
+			}
+		}
+	}
+}
