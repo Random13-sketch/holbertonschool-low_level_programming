42ec18a4e47a78cd502deb3cd81ce3a3de7f6120
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 13:43:51 +0400

    .

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - solves the problem
+ * @n: the input passed by user
+ *
+ * Return: solution of the problem
+ */
+
+
+int print_last_digit(int n)
+{
+	printf("%d", n % 10);
+	return (n % 10);
+}
