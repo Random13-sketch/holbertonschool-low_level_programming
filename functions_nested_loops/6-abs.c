2aba83d487a9a3da77e61bd52f780851a97e7fe3
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 13:42:18 +0400

    .

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - solves the problem
+ * @n: the input passed by user
+ *
+ * Return: solution of the problem
+ */
+
+
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
