f48be3aefe294ba5387bd4b70928a0de6389cfc2
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 13:56:27 +0400

    .

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,6 @@
 
 int print_last_digit(int n)
 {
-	_putchar((n < 0 ? (-n) % 10 : (n % 10)) + '0');
-	return (n < 0 ? (-n) % 10 : (n % 10));
+	_putchar((n < 0 ? (-((n) % 10)) : (n % 10)) + '0');
+	return (n < 0 ? (-((n) % 10)) : (n % 10));
 }
