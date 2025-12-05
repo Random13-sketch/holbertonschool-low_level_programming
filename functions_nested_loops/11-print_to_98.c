43b4ad655f86091fb0ff0191b827b4930151925f
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 14:17:46 +0400

    .

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - solves the problem
+ * @n: the input passed by user
+ *
+ */
+
+void print_to_98(int n);
+{
+	if (n >= 98)
+	{
+		int i;
+
+		for (i = n; i > 98; --i)
+		{
+			printf("%d, ", i);
+		}
+	}	
+	else
+	{
+		int i;
+
+		for (i = n; i < 98; ++i)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n");
+}
