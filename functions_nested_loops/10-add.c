812ca80e504a4c9b54daaa81509aff82789b1f50
Random13-sketch <agazadenahid98@gmail.com>
2025-12-05 14:12:41 +0400

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - solves the problem
+ * @a: the input passed by user
+ *
+ * @b - the second input passed by user
+ *
+ * Return: solution of the problem
+ */
+
+int add(int a, int b);
+{
+	return a + b;	
+}
