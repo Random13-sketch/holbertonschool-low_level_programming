8ed74b3f60f20eaddd2687b35f0c87b36ee3cb2e
Random13-sketch <agazadenahid98@gmail.com>
2025-12-07 18:44:32 +0000

    .

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,6 +1,35 @@
 #include "main.h"
 
 /**
+ * print_cell - prints a cell (after the first column) in the times table
+ * @n: value to print
+ */
+static void print_cell(int n)
+{
+	_putchar(',');
+	_putchar(' ');
+
+	if (n < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		_putchar(' ');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
+
+/**
  * print_times_table - prints the n times table, starting with 0
  * @n: the table size
  *
@@ -18,42 +47,10 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			p = i * j;
-
 			if (j == 0)
-			{
-				_putchar(p + '0');
-			}
+				_putchar('0');
 			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (p <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (p <= 99)
-				{
-					_putchar(' ');
-				}
-
-				if (p >= 100)
-				{
-					_putchar((p / 100) + '0');
-					_putchar(((p / 10) % 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-				else if (p >= 10)
-				{
-					_putchar((p / 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-				else
-				{
-					_putchar(p + '0');
-				}
-			}
+				print_cell(p);
 		}
 		_putchar('\n');
 	}
