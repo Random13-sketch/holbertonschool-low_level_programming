79bffe3fa88ce1ff42927ceb19625351a0a74725
Random13-sketch <agazadenahid98@gmail.com>
2025-12-10 13:38:33 +0400

    .

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of params a and b
+ * @a: pointer to the int
+ * @b: pointer to the int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
