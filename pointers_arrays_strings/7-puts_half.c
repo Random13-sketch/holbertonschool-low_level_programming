819c13d25354a5dbd6db39e7611bab54a29440e9
Random13-sketch <agazadenahid98@gmail.com>
2025-12-11 16:58:20 +0400

    fix: improve code readability in puts_half function by adding braces for clarity

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,23 @@ void puts_half(char *str)
 	int i;
 
 	while (str[len] != '\0')
-		len++;
+	{
+		++len;
+	}
 
     if (len % 2 == 0)
+	{
 		i = len / 2;
+	}
     else
+	{
 		i = (len + 1) / 2;
+	}
 
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i++;
+		++i;
 	}
 
 	_putchar('\n');
