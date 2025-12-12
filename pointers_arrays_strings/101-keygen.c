37ebbeb026f18a2d819170f945e631eb6608f8e6
Random13-sketch <agazadenahid98@gmail.com>
2025-12-12 12:42:19 +0400

    feat: enhance 101-keygen program with detailed comments and improved logic for password generation

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -4,16 +4,23 @@
 
 /**
  * main - prints the solution for the program 101-keygen.
- *        I found the passed 2772 using dogbolt.org decompiler.
+ *        I found the passed 2772. It denotes the sum of ascii values
+ *        of the password. I found it using dogbolt.org decompiler.
  *
  * Return: Always 0 (Success).
  */
 
 int main(void)
 {
-	srand(time(0));
-
-	printf("%d\n", rand());
+	int sum;
+	
+	sum = 2772;
+	while(sum > 'z')
+	{
+		printf("z");
+		sum -= 'z';
+	}
+	printf("%c\n", sum);
 
 	return (0);
 }
