757d1bec3098f0725ed1ed1837951e53ce83fac5
Random13-sketch <agazadenahid98@gmail.com>
2025-12-12 12:22:03 +0400

    feat: add initial implementation of the 101-keygen program

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the solution for the program 101-keygen.
+ *        I found the passed 2772 using dogbolt.org decompiler.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("2772");
+
+	return (0);
+}
