29d723eb5a328741a2d38f07bcec0e5674b1f4ee
Random13-sketch <agazadenahid98@gmail.com>
2025-12-12 12:15:37 +0400

    fix: improve function documentation for _strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest
- * @dest: the destination buffer
- * @src: the source string
+ * _strcpy - Copies the string pointed to by src to dest.
+ *           This includes the terminating null byte ('\0').
+ * @dest: Destination buffer.
+ * @src: Source string.
+ *
+ * Return: Pointer to dest.
  */
 char *_strcpy(char *dest, char *src)
 {
