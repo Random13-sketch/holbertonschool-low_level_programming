4bfdf3abb92317636f1334cc92a6cd34cc1e4ef0
Random13-sketch <agazadenahid98@gmail.com>
2025-12-14 14:20:25 +0000

    re-written source code.

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -2,13 +2,66 @@
 #include <stdio.h>
 
 /**
+ * print_hex_line - prints the hex part of one line
+ * @b: buffer
+ * @size: total size
+ * @i: start index of the line
+ */
+void print_hex_line(char *b, int size, int i)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			printf("%02x", (unsigned char)b[i + j]);
+		}
+		else
+		{
+			printf("  ");
+		}
+
+		if (j % 2 == 1)
+		{
+			printf(" ");
+		}
+	}
+}
+
+/**
+ * print_ascii_line - prints the ascii part of one line
+ * @b: buffer
+ * @size: total size
+ * @i: start index of the line
+ */
+static void print_ascii_line(char *b, int size, int i)
+{
+	int j;
+	unsigned char c;
+
+	for (j = 0; j < 10 && i + j < size; j++)
+	{
+		c = (unsigned char)b[i + j];
+		if (c >= 32 && c <= 126)
+		{
+			printf("%c", c);
+		}
+		else
+		{
+			printf(".");
+		}
+	}
+}
+
+/**
  * print_buffer - prints a buffer (10 bytes per line)
  * @b: buffer
  * @size: size of buffer
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i;
 
 	if (size <= 0)
 	{
@@ -19,38 +72,8 @@ void print_buffer(char *b, int size)
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
-
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				printf("%02x", (unsigned char)b[i + j]);
-			}
-			else
-			{
-				printf("  ");
-			}
-
-			if (j % 2 == 1)
-			{
-				printf(" ");
-			}
-		}
-
-		for (j = 0; j < 10 && i + j < size; j++)
-		{
-			unsigned char c = (unsigned char)b[i + j];
-
-			if (c >= 32 && c <= 126)
-			{
-				printf("%c", c);
-			}
-			else
-			{
-				printf(".");
-			}
-		}
-
+		print_hex_line(b, size, i);
+		print_ascii_line(b, size, i);
 		printf("\n");
 	}
 }
