55dc9bdfb3f85a8b4f7ba2690069cb75d5aca879
Random13-sketch <agazadenahid98@gmail.com>
2025-12-14 13:33:40 +0000

    solve "100-rot13.c"

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to encode
+ *
+ * Return: pointer to str
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (in[j] != '\0')
+		{
+			if (str[i] == in[j])
+			{
+				str[i] = out[j];
+				j = 52;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (str);
+}
