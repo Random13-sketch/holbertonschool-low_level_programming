9976771da352237fb14344afffebd6d7fa55f05c
Random13-sketch <agazadenahid98@gmail.com>
2025-12-19 13:43:21 +0400

    add factorial function to calculate the factorial of a number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a number
+ *
+ * @n: input number
+ * Return: factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (1);
+	}
+	
+	return (n * factorial(n - 1));
+}
