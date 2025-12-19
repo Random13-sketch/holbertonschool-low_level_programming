c6d29b39277e04716c01ba65d7735263e4a39c30
Random13-sketch <agazadenahid98@gmail.com>
2025-12-19 13:49:29 +0400

    add: implement _pow_recursion function to calculate the power of a number

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the power of a number
+ *
+ * @n: input number
+ * Return: factorial of the number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return -1;
+	}
+	if (y <= 0)
+	{
+		return 1;
+	}
+	if (y == 1)
+	{
+		return x;
+	}
+	return x * _pow_recursion(x, y - 1);
+}
