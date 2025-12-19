1088b68af0cdd57ed989259d17719721d69a5dca
Random13-sketch <agazadenahid98@gmail.com>
2025-12-19 14:35:16 +0400

    fix: replace _strlen_recursion with len function for palindrome check

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * len - prints the length of a string
+ *
+ * @s: pointer to the string
+ * Return: size of the string
+ */
+
+int len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + len(s + 1));
+}
+
+
+/**
  * checker - checks the input number from n to the base
  * @n: number is squared and compared against base
  * @base: base number to check
@@ -13,7 +31,7 @@ int checker(char *s, int n, int base)
 	{
 		return (1);
 	}
-	if (s[n] != s[_strlen_recursion(s) - n - 1])
+	if (s[n] != s[base - n - 1])
 	{
 		return (0);
 	}
@@ -29,5 +47,5 @@ int checker(char *s, int n, int base)
 
 int is_palindrome(char *s)
 {
-	return (checker(s, 0, _strlen_recursion(s)));
+	return (checker(s, 0, len(s)));
 }
