6c2f81b445349f7ecc1b87817669bdf92e102825
Random13-sketch <agazadenahid98@gmail.com>
2025-12-21 00:07:27 +0400

    fix: replace atoi with custom _atoi function for string to integer conversion

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
 /**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ * Return: the integer value
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
+}
+
+/**
  * main - multiplies two numbers passed as arguments.
  * @argc: argument count
  * @argv: argument vector
@@ -17,8 +48,8 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	product = num1 * num2;
 
 	printf("%d\n", product);
