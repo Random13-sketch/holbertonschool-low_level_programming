c4dc30b96c6596ebd4021ddd891b2c13a8ada7be
Random13-sketch <agazadenahid98@gmail.com>
2025-12-22 03:00:20 +0400

    add: implement _malloc_checked and _realloc functions for memory management

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _malloc_checked - allocates memory and checks for success
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         exits with status 98 if allocation fails
+ */
+
+void *_malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated memory
+ * @new_size: new size of the memory block
+ *
+ * Return: pointer to the reallocated memory block, or NULL on failure
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	if (ptr == NULL)
+	{
+		return (_malloc_checked(new_size));
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	void *new_ptr = _malloc_checked(new_size);
+
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	unsigned int copy_size = old_size < new_size ? old_size : new_size;
+
+	for (unsigned int i = 0; i < copy_size; i++)
+	{
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+
+	return (new_ptr);
+}
