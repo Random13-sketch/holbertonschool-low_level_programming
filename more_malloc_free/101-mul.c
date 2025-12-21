145ad43e6648d032b51800f1d209f318b9a4617d
Random13-sketch <agazadenahid98@gmail.com>
2025-12-22 03:22:02 +0400

    refactor: enhance error handling and validation in big number multiplication

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,7 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * print_error - prints Error and exits with 98
+ */
+void print_error(void)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
+}
 
 /**
  * _isdigit - checks if character is digit
@@ -30,58 +42,67 @@ int _strlen(char *s)
 }
 
 /**
+ * validate_number - checks if a string is a valid positive number
+ * @s: string to validate
+ */
+void validate_number(char *s)
+{
+	int i = 0;
+
+	if (!s || s[0] == '\0')
+		print_error();
+
+	for (; s[i]; i++)
+	{
+		if (!_isdigit(s[i]))
+			print_error();
+	}
+}
+
+/**
  * big_multiply - multiply two big number strings
  * @s1: the first big number string
  * @s2: the second big number string
+ * @len: output length of result array
  *
- * Return: the product big number string
+ * Return: pointer to digits array (each cell 0..9)
  */
-char *big_multiply(char *s1, char *s2)
+char *big_multiply(char *s1, char *s2, int *len)
 {
 	char *r;
-	int l1, l2, a, b, c, x;
+	int l1, l2, i, j, a, b, carry;
 
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
+	*len = l1 + l2;
+
+	r = malloc(sizeof(char) * (*len));
 	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
+		print_error();
 
-	for (l1--; l1 >= 0; l1--)
+	for (i = 0; i < *len; i++)
+		r[i] = 0;
+
+	for (i = l1 - 1; i >= 0; i--)
 	{
-		if (!_isdigit(s1[l1]))
+		a = s1[i] - '0';
+		carry = 0;
+
+		for (j = l2 - 1; j >= 0; j--)
 		{
-			free(r);
-			printf("Error\n"), exit(98);
+			b = s2[j] - '0';
+			carry += r[i + j + 1] + (a * b);
+			r[i + j + 1] = carry % 10;
+			carry /= 10;
 		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		r[i + j + 1] += carry;
 	}
+
 	return (r);
 }
 
-
 /**
- * main - multiply two big number strings
+ * main - multiply two positive numbers
  * @argc: the number of arguments
  * @argv: the argument vector
  *
@@ -90,25 +111,27 @@ char *big_multiply(char *s1, char *s2)
 int main(int argc, char **argv)
 {
 	char *r;
-	int a, c, x;
+	int len, i, started = 0;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
+		print_error();
 
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
+	validate_number(argv[1]);
+	validate_number(argv[2]);
+
+	r = big_multiply(argv[1], argv[2], &len);
+
+	for (i = 0; i < len; i++)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		if (r[i] != 0)
+			started = 1;
+		if (started)
+			_putchar(r[i] + '0');
 	}
-	if (!a)
+
+	if (!started)
 		_putchar('0');
+
 	_putchar('\n');
 	free(r);
 	return (0);
