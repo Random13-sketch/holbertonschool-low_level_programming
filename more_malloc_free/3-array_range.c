4346a4b52f7ce65186e570a984de92eccc8a898a
Random13-sketch <agazadenahid98@gmail.com>
2025-12-22 02:58:15 +0400

    refactor: replace malloc_checked with _malloc_checked in array_range function

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _malloc_checked - allocates memory and checks for success
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         exits with status 98 if allocation fails
+ */
+
+void *_malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
+
+/**
  * array_range - creates an array of integers from min to max
  * @min: minimum value
  * @max: maximum value
@@ -19,7 +38,7 @@ int *array_range(int min, int max)
 	}
 
 	size = max - min + 1;
-	arr = malloc_checked(size * sizeof(int));
+	arr = _malloc_checked(size * sizeof(int));
 
 	for (i = 0; i < size; i++)
 	{
