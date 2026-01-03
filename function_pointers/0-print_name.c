d6de2e3d262b1ce40de79f68bac1678b401ad718
Random13-sketch <agazadenahid98@gmail.com>
2026-01-03 14:12:40 +0400

    added betty-style documentation

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to be printed
+ * @f: pointer to the function that prints the name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
