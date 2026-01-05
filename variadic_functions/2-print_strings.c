91a14b27831399547f09b3db8b2ae2b6c43b7217
Random13-sketch <agazadenahid98@gmail.com>
2026-01-05 13:55:23 +0400

    created 2-print_strings.c file

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: string printed between strings
+ * @n: number of strings passed to the function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *s;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+	printf("\n");
+}
