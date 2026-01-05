d6be0f3e970c1dafd7fbbbd50708517f5ebe71de
Random13-sketch <agazadenahid98@gmail.com>
2026-01-05 14:16:29 +0400

    created 100-hello_world.asm

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,19 @@
+section .data
+	msg db "Hello, World", 10
+	len equ $ - msg
+
+section .text
+	global main
+
+main:
+	; write(1, msg, len)
+	mov rax, 1          ; syscall: write
+	mov rdi, 1          ; fd: stdout
+	mov rsi, msg        ; buf
+	mov rdx, len        ; count
+	syscall
+
+	; exit(0)
+	mov rax, 60         ; syscall: exit
+	xor rdi, rdi        ; status 0
+	syscall
