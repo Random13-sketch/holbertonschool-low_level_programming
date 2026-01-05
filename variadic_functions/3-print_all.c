6f49e457a2cb74d47c0853a54606289bbff7a99e
Random13-sketch <agazadenahid98@gmail.com>
2026-01-05 14:03:44 +0400

    made the 3-print_all.c shorter via helper functions.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,16 +1,61 @@
 #include "variadic_functions.h"
 
 /**
+ * p_char - prints a char argument
+ * @ap: pointer to the variadic argument list
+ * @sep: separator to print before the value
+ */
+void p_char(va_list *ap, char *sep)
+{
+	printf("%s%c", sep, (char)va_arg(*ap, int));
+}
+
+/**
+ * p_int - prints an int argument
+ * @ap: pointer to the variadic argument list
+ * @sep: separator to print before the value
+ */
+void p_int(va_list *ap, char *sep)
+{
+	printf("%s%d", sep, va_arg(*ap, int));
+}
+
+/**
+ * p_float - prints a float argument
+ * @ap: pointer to the variadic argument list
+ * @sep: separator to print before the value
+ */
+void p_float(va_list *ap, char *sep)
+{
+	printf("%s%f", sep, va_arg(*ap, double));
+}
+
+/**
+ * p_string - prints a string argument (prints (nil) if NULL)
+ * @ap: pointer to the variadic argument list
+ * @sep: separator to print before the value
+ */
+void p_string(va_list *ap, char *sep)
+{
+	char *s = va_arg(*ap, char *);
+
+	if (s == NULL)
+	{
+		s = "(nil)";
+	}
+
+	printf("%s%s", sep, s);
+}
+
+/**
  * print_all - prints anything, followed by a new line
  * @format: list of argument types passed to the function
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0;
+	unsigned int i;
 	char *sep = "";
-	char *s;
 
 	if (format == NULL)
 	{
@@ -19,36 +64,30 @@ void print_all(const char * const format, ...)
 	}
 
 	va_start(ap, format);
+	i = 0;
 
 	while (format[i] != '\0')
 	{
 		switch (format[i])
 		{
-			case 'c':
-				printf("%s%c", sep, (char)va_arg(ap, int));
-				sep = ", ";
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(ap, int));
-				sep = ", ";
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(ap, double));
-				sep = ", ";
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-
-				if (s == NULL)
-				{
-					s = "(nil)";
-				}
-
-				printf("%s%s", sep, s);
-				sep = ", ";
-				break;
-			default:
-				break;
+		case 'c':
+			p_char(&ap, sep);
+			sep = ", ";
+			break;
+		case 'i':
+			p_int(&ap, sep);
+			sep = ", ";
+			break;
+		case 'f':
+			p_float(&ap, sep);
+			sep = ", ";
+			break;
+		case 's':
+			p_string(&ap, sep);
+			sep = ", ";
+			break;
+		default:
+			break;
 		}
 		i++;
 	}
