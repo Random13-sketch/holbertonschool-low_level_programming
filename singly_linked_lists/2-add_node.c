f97c217df88783f12b9d24c941ddad08624ac40c
Random13-sketch <agazadenahid98@gmail.com>
2026-01-13 12:01:35 +0400

    feat: implement add_node function to add a new node to the linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_node - adds new node to linked list
+ * @head: pointer to the first node of the linked list
+ * @str: content of the link to add header
+ *
+ * Return: the number of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp = {head};
+
+	tmp->str = str;
+	tmp->len = strlen(str);
+	tmp->next = NULL;
+	(*head)->next = tmp;
+	return tmp;
+}
