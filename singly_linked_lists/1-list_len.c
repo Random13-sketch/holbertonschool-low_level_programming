aebd41054078cbab8eea4389f4fafe88e5578b5a
Random13-sketch <agazadenahid98@gmail.com>
2026-01-13 11:00:23 +0400

    feat: add list_len function to count elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - number of elements in a linked list
+ * @h: pointer to the first node of the linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+	while (h)
+	{
+		c += 1;
+		h = h->next;
+	}
+	return (c);
+}
