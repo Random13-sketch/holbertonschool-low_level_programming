15324fabacba0868cd8afe78f7ab6f32415ed0f7
Random13-sketch <agazadenahid98@gmail.com>
2026-01-14 21:40:59 +0400

    feat: add free_list function to free a list_t linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the linked list
+ *
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free(head->len);
+	free(head->str);
+	free_list(head->next);
+}
