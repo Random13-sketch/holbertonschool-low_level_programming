53382018aed192cbba93ae4aa4d158fc156e85ee
Random13-sketch <agazadenahid98@gmail.com>
2026-01-14 21:52:40 +0400

    feat: add main function to print a specific string

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * main - prints the requried string
+ *
+ * Return: 0 (Success)
+ * 
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+	return (0);
+}
