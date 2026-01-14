68c570c79f9d19850b17b80f63465aff554b1ef3
Random13-sketch <agazadenahid98@gmail.com>
2026-01-14 19:39:49 +0400

    feat: implement add_node_end function to append nodes to the end of the linked list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds new node to the end of the linked list
+ * @head: pointer to the first node of the linked list
+ * @str: content of the link to add header
+ *
+ * Return: the number of nodes
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	if ((*head) == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	while ((*head)->next != NULL)
+	{
+		(*head) = (*head)->next;
+	}
+	(*head)->next->str = strdup(str);
+	(*head)->next->len = _strlen(str);
+	(*head)->next->next = NULL;
+
+	return (head);
+}
