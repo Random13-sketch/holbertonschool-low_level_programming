e73459935d9538cc88672b469de0fd87293e5faa
Random13-sketch <agazadenahid98@gmail.com>
2026-01-15 05:26:02 +0400

    feat: implement add_dnodeint function to add a node to the doubly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - returns the number of elements in a linked dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: element to be added to the doubly linked list
+ *
+ * Return: the number of nodes
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp, *cur;
+
+	if (head == NULL || n == NULL)
+	{
+		return (NULL);
+	}
+
+	cur = *head;
+	tmp = malloc(sizeof(*tmp));
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+
+	tmp->n = n;
+	tmp->prev = NULL;
+	tmp->next = cur;
+	head = tmp;
+
+	return (head);
+}
