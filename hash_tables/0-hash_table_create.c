28e0d76c7e184d5f3276a1b1ebc2c18200cefbe7
Random13-sketch <agazadenahid98@gmail.com>
2026-01-24 04:40:18 +0400

    added betty-style documentation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table
+ *         or NULL if something went wrong
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
